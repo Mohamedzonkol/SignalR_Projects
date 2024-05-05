14aeb57becf6c5d6ab743b489985da3e9e1ff287
Mohamedzonkol <mo.zonkol@gmail.com>
2024-05-06 01:40:11 +0300

    add signalr

--- SignalR_Project/Hubs/UserHub.cs
+++ SignalR_Project/Hubs/UserHub.cs
@@ -4,5 +4,12 @@ namespace SignalR_Project.Hubs
 {
     public class UserHub : Hub
     {
+        public static int TotalViews { get; set; } = 0;
+
+        public async Task NewWindowLoaded(string message)
+        {
+            TotalViews++;
+            await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
+        }
     }
 }
