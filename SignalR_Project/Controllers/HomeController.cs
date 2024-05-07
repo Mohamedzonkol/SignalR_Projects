e5a504da144c28bdc73339eed1ab32c38a1e59d4
Mohamedzonkol <mo.zonkol@gmail.com>
2024-05-07 23:29:59 +0300

    Deathy Hallow

--- SignalR_Project/Controllers/HomeController.cs
+++ SignalR_Project/Controllers/HomeController.cs
@@ -1,23 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR_Project.Hubs;
 using SignalR_Project.Models;
 using System.Diagnostics;
 
 namespace SignalR_Project.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> logger, IHubContext<DeathlyHallowsHub> deathHub) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ILogger<HomeController> _logger = logger;
 
         public IActionResult Index()
         {
             return View();
         }
 
+        public async Task<IActionResult> DeathlyHallows(string type)
+        {
+            if (SD.DealthyHallowRace.ContainsKey(type))
+            {
+                SD.DealthyHallowRace[type]++;
+            }
+            await deathHub.Clients.All.SendAsync("updateDealthyHallowCount",
+                SD.DealthyHallowRace[SD.Cloak],
+                SD.DealthyHallowRace[SD.Stone],
+                SD.DealthyHallowRace[SD.Wand]
+                );
+            return Accepted();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
